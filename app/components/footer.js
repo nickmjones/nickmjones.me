be311d1679ef6f196dd4826d8d28a79592cf95f1
Nick Jones <nickmjones@nicks-air.myfiosgateway.com>
2023-11-04 14:41:20 -0400

    First

---
+++ app/components/footer.js
@@ -0,0 +1,9 @@
+const Footer = ({ children }) => {
+  return(
+    <footer className="px-8 py-8 md:py-16 md:px-24 text-sm text-slate-500 bg-slate-800">
+      Copyright &copy; 2023 Nick M. Jones • All rights reserved • Made with great love and care, and Javascript.
+    </footer>
+  )
+}
+
+export default Footer
---
+++ app/gear/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return(
+    <div class="p-32">
+      Gear Home
+    </div>
+  )
+}
